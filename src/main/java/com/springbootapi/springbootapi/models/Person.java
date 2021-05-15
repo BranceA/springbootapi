e3b410241bba66687bb151b78f0d2276db49d74d
BranceA <brancealexander@gmail.com>
2021-05-15 14:06:07 -0500

    updated maven file and added person class

---
+++ src/main/java/com/springbootapi/springbootapi/models/Person.java
@@ -0,0 +1,81 @@
+package com.springbootapi.springbootapi.models;
+
+import java.sql.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "people")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int age;
+
+    @Column
+    private Date dateJoined;
+
+    @Column
+    private Date dateUpdated;
+
+
+    public Person() {
+    }
+
+
+    public Person(long id, String name, int age, Date dateJoined, Date dateUpdated) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dateJoined = dateJoined;
+        this.dateUpdated = dateUpdated;
+    }
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getDateJoined() {
+        return this.dateJoined;
+    }
+
+    public void setDateJoined(Date dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    public Date getDateUpdated() {
+        return this.dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    
+}
