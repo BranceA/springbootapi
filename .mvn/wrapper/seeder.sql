0f5507a7a0630d3c64498cd229e6a170066500b2
BranceA <brancealexander@gmail.com>
2021-05-15 15:26:12 -0500

    added seeder file

---
+++ .mvn/wrapper/seeder.sql
@@ -0,0 +1,5 @@
+INSERT INTO people(age, date_joined, date_updated, name) 
+values(30, '2021-05-10', '2021-05-15', 'Bob Doe'),
+(45 '2021-05-02', '2021-05-15', 'Sally Doe'),
+(7, '2021-08-10', '2021-11-10', 'Billy Doe'), 
+(22, '2020-02-10', '2021-12-25', 'Bertha Doe');
