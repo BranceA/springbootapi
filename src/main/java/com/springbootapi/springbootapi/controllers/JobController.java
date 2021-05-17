0189960d462aa6536e388a3ccdbe2a2309d47564
BranceA <brancealexander@gmail.com>
2021-05-17 15:17:09 -0500

    added get mappings for jobs

--- src/main/java/com/springbootapi/springbootapi/controllers/JobController.java
+++ src/main/java/com/springbootapi/springbootapi/controllers/JobController.java
@@ -1,5 +1,9 @@
 package com.springbootapi.springbootapi.controllers;
 
+import javax.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import com.springbootapi.springbootapi.models.Job;
 import com.springbootapi.springbootapi.repositories.JobRepository;
 
@@ -13,6 +17,18 @@ public class JobController {
     public JobController(JobRepository jobDao) {
         this.jobDao = jobDao;
     }
+
+    // curl -v http://localhost:8080/job
+    @GetMapping("/job")
+    public List<Job> viewAllJobsList(){
+        return jobDao.findAll();
+    };
+
+    // curl -v http://localhost:8080/job/3
+    @GetMapping("/job/{id}")
+    public Job viewJobById(@PathVariable Long id){
+        return jobDao.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    };
 
 
     // curl -X POST localhost:8080/jobs -H 'Content-type:application/json' -d '{"jobTitle":"Code Tester","salary":3500}'
