da20e9f5cb1dbf10289cb05f67cfd42c45943af6
BranceA <brancealexander@gmail.com>
2021-05-18 11:29:19 -0500

    added a bunch of comments in controllers

--- src/main/java/com/springbootapi/springbootapi/controllers/PersonController.java
+++ src/main/java/com/springbootapi/springbootapi/controllers/PersonController.java
@@ -36,12 +36,14 @@ public class PersonController {
         return personDao.findAll();
     };
 
+    // Include person id in URL to make a get request for an individual person
     // curl -v http://localhost:8080/person/3
     @GetMapping("/person/{id}")
     public Person viewPersonById(@PathVariable Long id){
         return personDao.findById(id).orElseThrow(() -> new EntityNotFoundException());
     };
 
+    // Include job Id in URL and include person object in request to create a new person and give it an existing job
     // curl -X POST localhost:8080/person/5 -H 'Content-type:application/json' -d '{"name": "Dan Doe", "dateJoined": "2020-01-01", "dateUpdated": "2021-05-15", "age": 20}'
     @PostMapping("/person/{id}")
     Person newPerson(@RequestBody Person newPerson, @PathVariable Long id) {
@@ -50,12 +52,14 @@ public class PersonController {
         return personDao.save(newPerson);
     };
 
+    // For creating a new person with no job. Job will be null in database
     // curl -X POST localhost:8080/person -H 'Content-type:application/json' -d '{"name": "Helen Doe", "dateJoined": "2018-11-22", "dateUpdated": "2020-05-15", "age": 88}'
     @PostMapping("/person")
     Person newPerson(@RequestBody Person newPerson) {
         return personDao.save(newPerson);
     };
 
+    // For updating a person. Include the id of the person you're updating in the URL and send a person object in the request.
     // curl -X PUT localhost:8080/person/3 -H 'Content-type:application/json' -d '{"name": "William Doe", "dateJoined": "1920-01-01", "dateUpdated": "2021-05-15", "age": 98}'
     @PutMapping("/person/{id}")
     Person replacePerson(@RequestBody Person replacePerson, @PathVariable Long id){
@@ -68,12 +72,14 @@ public class PersonController {
         return personDao.save(person);
     }
 
+    // Include id of the person you want to delete in the URL
     // curl -X DELETE localhost:8080/person/5
     @DeleteMapping("/person/{id}")
         void deletePerson(@PathVariable Long id) {
         personDao.deleteById(id);
     }
 
+    // For adding a new job to an existing person. Include person id in the URL and send job object in the request.
     // curl -X PUT localhost:8080/test/6 -H 'Content-type:application/json' -d '{"jobTitle":"Actual Dragon","salary":250}'
     @PutMapping("/addJob/{id}")
     Person updateJob(@RequestBody Job job, @PathVariable Long id){
